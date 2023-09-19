a54641a960db245f826fd165b48afee6a72095a7
Michael Mironov <nemiron94@gmail.com>
2023-09-19 11:44:10 +0300

    Add read/write external storage permissions

--- app/src/androidTest/java/com/example/kaspresso/tests/BaseTest.kt
+++ app/src/androidTest/java/com/example/kaspresso/tests/BaseTest.kt
@@ -1,10 +1,18 @@
 package com.example.kaspresso.tests
 
+import android.Manifest
+import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
 
 open class BaseTest : TestCase(
     kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()
 ) {
+    @get: Rule
+    val runtimeException: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 }
