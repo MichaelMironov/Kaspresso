8d6e77cbd56c31e92ecc25f39966083f507de7de
MichaelMironov <nemiron94@gmail.com>
2023-09-19 20:25:26 +0300

    Move scenario rule to base class

--- app/src/androidTest/java/com/example/kaspresso/tests/BaseTest.kt
+++ app/src/androidTest/java/com/example/kaspresso/tests/BaseTest.kt
@@ -1,7 +1,9 @@
 package com.example.kaspresso.tests
 
 import android.Manifest
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.rule.GrantPermissionRule
+import com.example.kaspresso.MainActivity
 import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -10,6 +12,10 @@ import org.junit.Rule
 open class BaseTest : TestCase(
     kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()
 ) {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
     @get: Rule
     val runtimeException: GrantPermissionRule = GrantPermissionRule.grant(
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
