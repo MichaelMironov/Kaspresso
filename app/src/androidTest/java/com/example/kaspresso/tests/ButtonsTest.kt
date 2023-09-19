6a42c28ac91d5e68a5f61c00ddba2e141f887e92
MichaelMironov <nemiron94@gmail.com>
2023-09-19 20:26:46 +0300

    Move tests to buttons test

--- app/src/androidTest/java/com/example/kaspresso/tests/ButtonsTest.kt
+++ app/src/androidTest/java/com/example/kaspresso/tests/ButtonsTest.kt
@@ -1,21 +1,34 @@
 package com.example.kaspresso.tests
 
-import androidx.test.ext.junit.rules.activityScenarioRule
-import com.example.kaspresso.MainActivity
 import com.example.kaspresso.screens.MainScreen
 import io.qameta.allure.kotlin.junit4.DisplayName
-import org.junit.Rule
 import org.junit.Test
 
-class IncrementButtonTest : BaseTest() {
-
-    @get:Rule
-    val activityRule = activityScenarioRule<MainActivity>()
+class ButtonsTest : BaseTest() {
 
     @Test
     @DisplayName("[UNIT] Increase count")
     fun incrementButtonShouldIncreaseCounter() = run {
         step("Increase counter to 2") { MainScreen { incrementButton.doubleClick() } }
         step("Counter should be equal 2") { MainScreen { counterText.hasText("Count: 2") } }
+    }
+
+    @Test
+    @DisplayName("[UNIT] Decrease count")
+    fun decrementButtonDecreaseCounter() = run {
+        step("Decrease counter value to '-4'") {
+            MainScreen { repeat(4) { decrementButton.click() } }
+        }
+        step("Counter should equal '-4'") {
+            MainScreen { counterText.hasText("Count: -4") }
+        }
+    }
+
+    @Test
+    @DisplayName("[UNIT] Reset count")
+    fun clearButtonResetCount() = run {
+        step("Increase counter value to '2") { MainScreen.incrementButton.doubleClick() }
+        step("Click button 'Clear'") { MainScreen.clearButton.click() }
+        step("Counter should equal '0'") { MainScreen { counterText.hasText(0) } }
     }
 }
